b5ddac765faba5ab21495dd8174a37f6ae2e2575
soloshowdown <ushinkarkunal@gmail.com>
2025-06-27 20:19:13 +0530

    LC 704

---
+++ leetcode/arr4.cpp
@@ -0,0 +1,12 @@
+//binary search LC 704
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==target){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
