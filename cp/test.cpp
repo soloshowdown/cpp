8ac7742ab531891fd7127ddf4a5f90e0482d8749
soloshowdown <ushinkarkunal@gmail.com>
2026-01-26 20:52:40 +0530

    test

---
+++ cp/test.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// ios::sync_with_stdio(0);
+// cin.tie(0);
+
+int main() {
+    // cout<<"Hello World!"<<endl;
+    // int a,b;
+    // string x;
+    // cin>>a>>b>>x;
+    // cout<<a<<" "<<b<<" "<<x<<"\n";
+
+    // string s;
+    // getline(cin, s);
+    // cout<< s<<"\n";
+
+}
+// newline "\n" works faster than "endl"
+
+// for case of input and output files given (beginning of code)
+// freopen("input.txt", "r" , stdin);
+// freopen("output.txt", "w" , stdout);
