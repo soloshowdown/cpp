5ce858e148f27c39a6c8e892e1d6f7666c13a038
soloshowdown <ushinkarkunal@gmail.com>
2026-01-26 23:11:16 +0530

    numbers

---
+++ cp/numbers.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+ int main() {
+     //using command "typedef" to give shorter names to datatypes
+     typedef long long ll;
+     ll a = 123456789;
+     ll b = 987654321;
+     cout<< a*b << "\n";
+
+     //can be done w vectors and pairs like:
+     // typedef vector<int> vi
+     // typedef pair<int,int> pi
+ }
