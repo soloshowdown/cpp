53b5ae78e5ea7c4857b8435b2fcc21730a1f9c00
soloshowdown <ushinkarkunal@gmail.com>
2026-01-26 23:37:17 +0530

    macros

---
+++ cp/macros.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define SQ(a) (a)*(a)
+// #define F first
+// #define S second
+// #define PB push_back
+// #define MP make_pair
+int main() {
+
+//
+//     int x1,x2=0,y1,y2;
+//     v.PB(MP(y1,x1));
+//     v.PB(MP(y2,x2));
+//     int d = v[i].F+v[i].S;
+//
+
+    cout<<SQ(3+3)<< "\n";
+    return 0;
+
+
+}
