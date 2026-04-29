02a70c2ea038cd6f837fa5a0a0025cf2416fe6ca
soloshowdown <ushinkarkunal@gmail.com>
2026-04-29 20:54:49 +0530

    800 lvl

---
+++ cfproblems/617A.cpp
@@ -0,0 +1,13 @@
+//
+// Created by kunal on 29-04-2026.
+//
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    long long int n;
+    cin>>n;
+    long long int count=0;
+    if (n%5 == 0) cout<< n/5 << endl;
+    else cout<< n/5 +1 << endl;
+    return 0;
+}
