69a7acfae88d9b3dd26660ee62c8f3b533b49229
soloshowdown <ushinkarkunal@gmail.com>
2026-09-01 23:55:47 +0530

    200B

---
+++ cfproblems/200B.cpp
@@ -0,0 +1,17 @@
+//
+// Created by kunal on 01-09-2026.
+//
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+     int n, a[110];
+     cin >> n;
+     double ans=0.0, sum=0.0;
+     for (int i = 0; i < n; i++) {
+          cin >> a[i];
+          sum+=a[i];
+     }
+     ans= sum/n;
+     cout << fixed << setprecision(12)<<ans<<"\n";
+     return 0;
+}
